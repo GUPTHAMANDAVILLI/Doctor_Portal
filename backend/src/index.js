6166b22289442304247a474567bdc6377946dc63
GUPTHAMANDAVILLI <sarabhayya.guptha@gmail.com>
2026-08-02 20:00:20 +0530

    GitHub Pages deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:4200',
+  'http://localhost:3000',
+  'https://gupthamandavilli.github.io'
+];
+
 app.use(cors({
-  origin: ['http://localhost:4200', 'http://localhost:3000'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
